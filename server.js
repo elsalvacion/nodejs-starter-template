cfb814c08ff946148671b29738fa43031aa813fb
elsalvacion <elsalvacion2022@gmail.com>
2021-09-05 01:59:12 +0600

    security packages added

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require("express");
+const mongoSanitize = require("express-mongo-sanitize");
+const cors = require("cors");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+
+// config
 const connectDB = require("./config/db");
 
 // require and use config files
@@ -6,6 +13,13 @@ require("dotenv").config({ path: "./config/.env" });
 connectDB();
 
 const app = express();
+
+// external middlewares
+app.use(mongoSanitize());
+app.use(cors());
+app.use(helmet());
+app.use(xss());
+app.use(hpp());
 
 // express body parser
 
